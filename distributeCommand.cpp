acd1c56402f80b5b80aa930ace206ba34bb2b592
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2024-07-13 22:42:42 -0400

    fix2

--- distributeCommand.cpp
+++ distributeCommand.cpp
@@ -1,17 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+
 #define WINDOW_SIZE 4440
 
-class connection{
+class connection {
     private:
         int vmID;
         char currentWindow[WINDOW_SIZE];
-        char auth[256];
+        std::string auth;
     public:
-    connection(int vmID, char *auth){
-        if(strcmp(auth, this->auth) != 0){
-            exit(2); //bad auth -> (somone is trying a brute force ... we use an internal secret key to prevent this. good luck cracking 256^256)
+        connection(int vmID, const char* auth) : vmID(vmID), auth("setForprod") {
+            if (strcmp(auth, this->auth.c_str()) != 0) {
+                exit(2); // bad auth
+            }
         }
-    }
+
+        void runCommand(const char* command) {
+            std::string fullCommand = "lxc exec " + std::to_string(this->vmID) + " -- " + command;
+            system(fullCommand.c_str());
+        }
 };
