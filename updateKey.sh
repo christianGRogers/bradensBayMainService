6ffe35af66b284c98899715fb6db6efd671e056b
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2024-10-14 19:57:05 -0400

    tyhny

--- updateKey.sh
+++ updateKey.sh
@@ -12,6 +12,13 @@ PUBLIC_KEY=$2
 
 
 # Write the public key to the authorized_keys file inside the container
-lxc exec -u christian $USER_ID bash -c "mkdir -p ~/.ssh && echo \"$PUBLIC_KEY\" >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys && chmod 700 ~/.ssh"
+lxc exec $USER_ID -- bash -c "
+    NON_ROOT_USER=\$(ls /home | head -n 1) && \
+    mkdir -p /home/\$NON_ROOT_USER/.ssh && \
+    echo \"$PUBLIC_KEY\" >> /home/\$NON_ROOT_USER/.ssh/authorized_keys && \
+    chmod 600 /home/\$NON_ROOT_USER/.ssh/authorized_keys && \
+    chmod 700 /home/\$NON_ROOT_USER/.ssh && \
+    chown -R \$NON_ROOT_USER:\$NON_ROOT_USER /home/\$NON_ROOT_USER/.ssh"
+
 
 echo "Public key added to container $USER_ID."
