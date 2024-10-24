e2587dd1d35bb7f5cfb72519513f0f5bcb05328a
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2024-10-24 18:53:08 -0400

    dcs

--- updateKey.js
+++ updateKey.js
@@ -3,7 +3,6 @@ const { exec } = require('child_process');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
 const app = express();
 const port = 3002;
 app.use(cors());
@@ -11,44 +10,32 @@ app.use(bodyParser.json());
 
 app.post('/addkey', (req, res) => {
     const { uid, key } = req.body;
-    console.log(uid);
-    console.log(key);w
 
     if (!uid || !key) {
         return res.status(400).json({ error: 'USER_ID and PUBLIC_KEY are required.' });
     }
 
-    // Sanitize inputs to prevent command injection
     const sanitizedUid = uid.replace(/[^a-zA-Z0-9_-]/g, '');
-    const sanitizedKey = key.replace(/[^a-zA-Z0-9@:.+\/= -]/g, '');
+    const sanitizedKey = key.replace(/[^a-zA-Z0-9@:.+\/= -]/g, '').replace(/"/g, '\\"');
 
     const command = `
-        #!/bin/bash
-
         USER_ID="${sanitizedUid}"
-        PUBLIC_KEY="${sanitizedKey}"
-
-        # Write the public key to the authorized_keys file inside the container
         lxc exec $USER_ID -- bash -c "
             NON_ROOT_USER=\$(ls /home | head -n 1) && \
             mkdir -p /home/\$NON_ROOT_USER/.ssh && \
-            echo \"$PUBLIC_KEY\" >> /home/\$NON_ROOT_USER/.ssh/authorized_keys && \
+            echo \"${sanitizedKey}\" >> /home/\$NON_ROOT_USER/.ssh/authorized_keys && \
             chmod 600 /home/\$NON_ROOT_USER/.ssh/authorized_keys && \
             chmod 700 /home/\$NON_ROOT_USER/.ssh && \
             chown -R \$NON_ROOT_USER:\$NON_ROOT_USER /home/\$NON_ROOT_USER/.ssh"
     `;
+    console.log(`Executing command: ${command}`);
 
-    // Execute the bash command
     exec(command, (error, stdout, stderr) => {
         if (error) {
-            console.error(`Error adding key: ${error.message}`);
+            console.error(`Execution error: ${error.message}`);
+            console.error(`stderr: ${stderr}`);
             return res.status(500).json({ error: 'Failed to add key to the container.' });
         }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return res.status(500).json({ error: 'Error occurred during key addition.' });
-        }
-
         console.log(`stdout: ${stdout}`);
         return res.status(200).json({ message: `Public key added to container ${sanitizedUid}.` });
     });
@@ -57,4 +44,3 @@ app.post('/addkey', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
