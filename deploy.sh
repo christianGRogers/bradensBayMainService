9f1d95b037b376947fd2d499bdd02f3c6ca5dffe
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2024-11-01 23:26:46 -0400

    fuxck

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #/bin/bash
 
+sudo chmod +x newUserSchedular.sh
+sudo chmod +x newUser.sh
+sudo screen -dmS runPromptSession DEBUG=* node runPrompt.js
+sudo screen -dmS startVmSession DEBUG=* node startVm.js
+sudo screen -dmS updateKeySession DEBUG=* node updateKey.js
+echo "started api\'s"
 
-if [ "$HOSTNAME" -eq "testservera" ]; then
-    sudo chmod +x newUserSchedular.sh
-    sudo chmod +x newUser.sh
-    sudo screen -dmS runPromptSession DEBUG=* node runPrompt.js
-    sudo screen -dmS startVmSession DEBUG=* node startVm.js
-    sudo screen -dmS updateKeySession DEBUG=* node updateKey.js
-    echo "started api\'s"
-fi
