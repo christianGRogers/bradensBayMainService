dcfcf2e263d69e83d511620baa362895c5e5399b
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2024-11-01 23:21:13 -0400

    sdc

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,5 @@ if [ "$HOSTNAME" = "testservera" ]; then
     sudo screen -dmS runPromptSession DEBUG=* node runPrompt.js
     sudo screen -dmS startVmSession DEBUG=* node startVm.js
     sudo screen -dmS updateKeySession DEBUG=* node updateKey.js
+    echo started api's
 fi
