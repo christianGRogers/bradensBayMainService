398f2a8187ef1c6e841f0ca576ceaaa9148fad76
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2024-11-01 23:15:03 -0400

    kfjmdln g

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+
+if [ "$HOSTNAME" = "testservera" ]; then
+    sudo chmod +x newUserSchedular.sh
+    sudo chmod +x newUser.sh
+    sudo screen -dmS runPromptSession DEBUG=* node runPrompt.js
+    sudo screen -dmS startVmSession DEBUG=* node startVm.js
+    sudo screen -dmS updateKeySession DEBUG=* node updateKey.js
+fi
