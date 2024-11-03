be1440b069a5c4e008387caff5f37b97c6c34cc9
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2024-11-03 15:41:37 -0500

    sac

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #/bin/bash
 
-
+sudo npm install @google/generative-ai
+sudo npm install cors
+sudo npm install firebase
+sudo npm install express
 sudo screen -dmS runPromptSession bash -c 'DEBUG=* node runPrompt.js' || echo "Failed to start runPromptSession" >> deploy.log
 sudo screen -dmS startVmSession bash -c 'DEBUG=* node startVm.js' || echo "Failed to start startVmSession" >> deploy.log
 sudo screen -dmS updateKeySession bash -c 'DEBUG=* node updateKey.js' || echo "Failed to start updateKeySession" >> deploy.log
