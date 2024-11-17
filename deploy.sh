c640f49b4292833c9b7076ee55be66f664873d0d
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2024-11-16 20:01:01 -0500

    ilhj

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 sudo git clone https://github.com/christianGRogers/bradensbay-start-vm-api
+sudo git clone https://github.com/christianGRogers/bradensbay-update-key-api
+sudo git clone https://github.com/christianGRogers/bradensbay-run-prompt-api
 sudo git clone 
 sudo chmod +x newUserSchedular.sh
 sudo chmod +x newUser.sh
