522e471e2357c5a3029c48d1c5082fd5c3c4d776
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2024-11-16 22:17:00 -0500

    sdfvb

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,15 @@ cd app
 sudo git clone https://github.com/christianGRogers/bradensbay-start-vm-api
 sudo git clone https://github.com/christianGRogers/bradensbay-update-key-api
 sudo git clone https://github.com/christianGRogers/bradensbay-run-prompt-api
-sudo git clone 
 sudo chmod +x bradensbay-start-vm-api/newUserSchedular.sh
 sudo chmod +x bradensbay-start-vm-api/newUser.sh
 sudo npm install @google/generative-ai
 sudo npm install cors
 sudo npm install firebase
 sudo npm install express
-sudo screen -dmS runPromptSession bash -c 'DEBUG=* node bradensbay-run-prompt-api/runPrompt.js' >> deploy.log
-sudo screen -dmS startVmSession bash -c 'DEBUG=* node bradensbay-start-vm-api/startVm.js' >> deploy.log
-sudo screen -dmS updateKeySession bash -c 'DEBUG=* node bradensbay-update-key-api/updateKey.js' >> deploy.log
+sudo screen -dmS runPromptSession bash -c 'DEBUG=* node bradensbay-run-prompt-api/index.js' >> deploy.log
+sudo screen -dmS startVmSession bash -c 'DEBUG=* node bradensbay-start-vm-api/index.js' >> deploy.log
+sudo screen -dmS updateKeySession bash -c 'DEBUG=* node bradensbay-update-key-api/index.js' >> deploy.log
 echo "API processes started in screen sessions."
 
 
